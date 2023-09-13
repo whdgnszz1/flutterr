2fb9d870f21fff568625428c702c3104be6f219d
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-14 00:19:23 +0900

    Refactor: Refactor WebViewWidget ver 3.0.0 to ver 4.0.2

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutterr/screen/home_screen.dart';
 
 void main() {
+  // Flutter 프래임워크가
+  // 앱을 실행할 준비가 될때까지 기다린다.
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     MaterialApp(
       home: HomeScreen(),
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+
 class HomeScreen extends StatelessWidget {
-  WebViewController? controller;
-  final homeUrl = 'https://blog.codefactory.ai';
+  WebViewController controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..loadRequest(homeUrl);
 
   HomeScreen({Key? key}) : super(key: key);
 
@@ -17,10 +20,7 @@ class HomeScreen extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {
-              if (controller == null) {
-                return;
-              }
-              controller!.loadUrl(homeUrl);
+              controller.loadRequest(homeUrl);
             },
             icon: Icon(
               Icons.home,
@@ -28,13 +28,16 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: WebView(
-        onWebViewCreated: (WebViewController controller) {
-          this.controller = controller;
-        },
-        initialUrl: homeUrl,
-        javascriptMode: JavascriptMode.unrestricted,
+      body: WebViewWidget(
+        controller: controller,
       ),
+      // WebView(
+      //   onWebViewCreated: (WebViewController controller) {
+      //     this.controller = controller;
+      //   },
+      //   initialUrl: homeUrl,
+      //   javascriptMode: JavascriptMode.unrestricted,
+      // ),
     );
   }
 }
