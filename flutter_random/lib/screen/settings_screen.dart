5388e09cb9817cf3a0ab21448d8e9f7b7b0e909d
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-15 00:47:31 +0900

    Refactor: Refactor settings screen

--- flutter_random/lib/screen/settings_screen.dart
+++ flutter_random/lib/screen/settings_screen.dart
@@ -21,49 +21,92 @@ class _SettingsScreenState extends State<SettingsScreen> {
             horizontal: 16.0,
           ),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: Row(
-                  children: maxNumber
-                      .toInt()
-                      .toString()
-                      .split("")
-                      .map(
-                        (e) => Image.asset(
-                          'asset/img/$e.png',
-                          width: 50,
-                          height: 70,
-                        ),
-                      )
-                      .toList(),
-                ),
+              _Body(
+                maxNumber: maxNumber,
               ),
-              Slider(
-                value: maxNumber,
-                min: 1000,
-                max: 1000000,
-                onChanged: (double val) {
-                  setState(() {
-                    maxNumber = val;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop(
-                    maxNumber.toInt(),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: RED_COLOR,
-                ),
-                child: Text('저장!'),
+              _Footer(
+                maxNumber: maxNumber,
+                onSliderChanged: onSliderChanged,
+                onButtonPressed: onButtonPressed,
               )
             ],
           ),
         ),
       ),
+    );
+  }
+
+  void onSliderChanged(double val) {
+    setState(() {
+      maxNumber = val;
+    });
+  }
+
+  void onButtonPressed() {
+    Navigator.of(context).pop(
+      maxNumber.toInt(),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final double maxNumber;
+
+  const _Body({required this.maxNumber, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        children: maxNumber
+            .toInt()
+            .toString()
+            .split("")
+            .map(
+              (e) => Image.asset(
+                'asset/img/$e.png',
+                width: 50,
+                height: 70,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final double maxNumber;
+  final ValueChanged<double>? onSliderChanged;
+  final VoidCallback onButtonPressed;
+
+  const _Footer({
+    required this.maxNumber,
+    required this.onSliderChanged,
+    required this.onButtonPressed,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Slider(
+          value: maxNumber,
+          min: 1000,
+          max: 1000000,
+          onChanged: onSliderChanged,
+        ),
+        ElevatedButton(
+          onPressed: onButtonPressed,
+          style: ElevatedButton.styleFrom(
+            primary: RED_COLOR,
+          ),
+          child: Text('저장!'),
+        )
+      ],
     );
   }
 }
